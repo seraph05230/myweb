379bb32b2ffd4a31e482440ee84eed133c517831
Tingwei <56641939+seraph05230@users.noreply.github.com>
2024-01-19 17:29:25 +0800

    Create ExternalUserPasswordService.java

---
+++ ExternalUserPasswordService.java
@@ -0,0 +1,8 @@
+package com.tingweichen.applicationsystem.service;
+
+import com.tingweichen.applicationsystem.model.ExternalUser;
+
+public interface ExternalUserPasswordService {
+
+    Integer createExternalUserPasswordInfo(ExternalUser externalUser);
+}
