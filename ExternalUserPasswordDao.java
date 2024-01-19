60016b52d66e0566575b5ebaa04bd48501a95079
Tingwei <56641939+seraph05230@users.noreply.github.com>
2024-01-19 17:26:55 +0800

    Create ExternalUserPasswordDao.java

---
+++ ExternalUserPasswordDao.java
@@ -0,0 +1,8 @@
+package com.tingweichen.applicationsystem.dao;
+
+import com.tingweichen.applicationsystem.model.ExternalUser;
+
+public interface ExternalUserPasswordDao {
+
+    Integer createExternalUserPasswordInfo(ExternalUser externalUser);
+}
